48dee3566f7f0369533c9d1ce42a1bb7a4deef8f
kota <>
2023-11-20 21:02:07 +0900

    Revert "[update] link with carousel app"
    This reverts commit f09e235565ed341be2eb06de9666cc8a01f2370a.

--- dst/js/include/top/top_carousel.js
+++ dst/js/include/top/top_carousel.js
@@ -1,33 +1,31 @@
 
-export function swiperInit() {
-
-  const defaultOptions = {
+(function(){
+const defaultOptions = {
     // ドットインジケーターの表示
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     // 前後スライドボタンを表示
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     loop: true, // ループの有効化
-    speed: 300,
+    speed: 600,
     autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    loopedSlides: 6,
-    centeredSlides: true,
-    loop: true,
-    spaceBetween: 30,
-  }
-  // デフォルトの表示
-  const swiper = new Swiper(".sample-swiper .swiper-container", {
-    ...defaultOptions,
-  });
-  return;
+        delay: 300,
+        disableOnInteraction: false,
+      },
+      slidesPerView: "auto",
+      loopedSlides: 6,
+      centeredSlides: true,
+      loop:true,
+      spaceBetween: 30,
 }
 
+// デフォルトの表示
+const swiper = new Swiper(".sample-swiper .swiper-container", {
+    ...defaultOptions,
+});
+})();
