96d398d093e184793488a473a2706082015be01c
kota <>
2023-12-02 21:18:00 +0900

    test

--- dst/js/include/top/top_carousel.js
+++ dst/js/include/top/top_carousel.js
@@ -1,31 +1,33 @@
 
-(function(){
-const defaultOptions = {
+export function swiperInit() {
+
+  const defaultOptions = {
     // ドットインジケーターの表示
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      el: ".swiper-pagination",
+      clickable: true,
     },
     // 前後スライドボタンを表示
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     loop: true, // ループの有効化
-    speed: 600,
+    speed: 300,
     autoplay: {
-        delay: 300,
-        disableOnInteraction: false,
-      },
-      slidesPerView: "auto",
-      loopedSlides: 6,
-      centeredSlides: true,
-      loop:true,
-      spaceBetween: 30,
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    loopedSlides: 6,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 30,
+  }
+  // デフォルトの表示
+  const swiper = new Swiper(".sample-swiper .swiper-container", {
+    ...defaultOptions,
+  });
+  return;
 }
 
-// デフォルトの表示
-const swiper = new Swiper(".sample-swiper .swiper-container", {
-    ...defaultOptions,
-});
-})();
--- laravel/src/resources/views/top.blade.php
+++ laravel/src/resources/views/top.blade.php
@@ -6,20 +6,37 @@
     <meta name="viewport" content="width=device-width">
     <title>テスト</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" />
-    <link rel="stylesheet" href="{{ asset('/css/index.css') }}"> />
+    <link rel="stylesheet" href="/css/index.css" />
+    <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap-switch-button@1.1.0/css/bootstrap-switch-button.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap-switch-button@1.1.0/dist/bootstrap-switch-button.min.js"></script>
 </head>
 
 <body>
     <!-- header -->
     @include('include.header')
     <!-- /header -->
+    <div>
+        <div class="p-4 text-end">
+            <input class="switch_button" data-switch-id="carousel" type="checkbox" data-toggle="switchbutton" checked data-size="lg" checked data-onlabel="PHP_USE" data-offstyle="success" data-offlabel="JS_USE">
+        </div>
+        <div class="switch_ON">
+            <!-- top_carousel -->
+            @include('include.top.top_carousel')
+            <!-- /header -->
+        </div>
+        <div class="switch_OFF">
+            <!-- top_carousel -->
+            @include('include.top.top_carousel')
+            <!-- /header -->
+        </div>
+    </div>
 
-    <!-- top_carousel -->
-    @include('include.top.top_carousel')
-    <!-- /header -->
     <!-- top_notice -->
     @include('include.top.top_notice')
     <!-- /header -->
+    <!-- commonJS -->
+    @include('include.commonJS')
+    <!-- /commonJS -->
 </body>
 
 </html>
