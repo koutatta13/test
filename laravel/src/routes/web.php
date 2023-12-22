2e8ec88a33581e02bb1c498e4e7781def92ba1f9
kota <>
2023-12-22 21:56:07 +0900

    [update] fix path

--- laravel/src/routes/web.php
+++ laravel/src/routes/web.php
@@ -6,11 +6,10 @@ use Illuminate\Http\Request;
 
 
 
-Route::view('/', 'top');
-Route::view('/top', 'top');
+Route::get('/',  'App\Http\Controllers\topController@index');
+Route::get('/top',  'App\Http\Controllers\topController@index');
 Route::get('/hello',  'App\Http\Controllers\HelloController@index');
 Route::get('/send',  'App\Http\Controllers\TestController@send');
 Route::post('/response',  'App\Http\Controllers\TestController@response');
 
-Route::get('/test',  'App\Http\Controllers\FirebaseController@loadDoc');
-Route::get('/test2',  'App\Http\Controllers\topController@index');
+//Route::get('/test',  'App\Http\Controllers\FirebaseController@loadDoc'); firebase TEST
