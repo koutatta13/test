3888b1130dd3b1270df777be4e58e71f5b7fc5b0
kota <>
2023-12-22 23:01:08 +0900

    [update] update text

--- laravel/src/resources/views/include/top/top_notice.blade.php
+++ laravel/src/resources/views/include/top/top_notice.blade.php
@@ -1,24 +1,43 @@
 <link rel="stylesheet" href="/css/include/top/top_notice.css">
 <div class="noticeCard">
 	<ul class="noticeCard_list">
-		<li class="noticeCard_list_item">
-			<a href="">
+	<li class="noticeCard_list_item">
+			<a href="https://www.cyberagent.co.jp/news/detail/id=29467">
 				<div class="noticeCard_imgBox">
 					<div class="noticeCard_imgBox_inner">
-						<img alt="" src="https://placehold.jp/24/0766AD/ffffff/310x175.png?text=画像1">
+						<img alt="" src="https://placehold.jp/24/C5E898/ffffff/310x175.png?text=画像2">
 					</div>
 				</div>
 				<div class="noticeCard_list_body">
 					<div class="noticeCard_body_meta">
-						<p class="noticeCard_body_badge">プレスリリース</p>
+						<p class="noticeCard_body_badge">機能実装</p>
 						<p class="noticeCard_body_date">
-							<time datetime="2023-11-02">2023年11月 2日</time>
+							<time datetime="2023-10-26">2023年12月18日</time>
 						</p>
 					</div>
-					<p class="noticeCard_body_text">製作した機能を書く</p>
+					<p class="noticeCard_body_text">ヴィジュアルリグレッションテストの作成</p>
 				</div>
 			</a>
 		</li>
+	<li class="noticeCard_list_item">
+			<a href="https://www.cyberagent.co.jp/news/detail/id=29467">
+				<div class="noticeCard_imgBox">
+					<div class="noticeCard_imgBox_inner">
+						<img alt="" src="https://placehold.jp/24/C5E898/ffffff/310x175.png?text=画像2">
+					</div>
+				</div>
+				<div class="noticeCard_list_body">
+					<div class="noticeCard_body_meta">
+						<p class="noticeCard_body_badge">機能実装</p>
+						<p class="noticeCard_body_date">
+							<time datetime="2023-10-26">2023年12月18日</time>
+						</p>
+					</div>
+					<p class="noticeCard_body_text">react×firebase カルーセル登録用のadminツールの制作</p>
+				</div>
+			</a>
+		</li>
+		
 		<li class="noticeCard_list_item">
 			<a href="">
 				<div class="noticeCard_imgBox">
@@ -28,32 +47,34 @@
 				</div>
 				<div class="noticeCard_list_body">
 					<div class="noticeCard_body_meta">
-						<p class="noticeCard_body_badge">プレスリリース</p>
+						<p class="noticeCard_body_badge">機能実装</p>
 						<p class="noticeCard_body_date">
-							<time datetime="2023-11-01">2023年11月 1日</time>
+							<time datetime="2023-11-01">2023年12月18日</time>
 						</p>
 					</div>
-					<p class="noticeCard_body_text">製作した機能を書く</p>
+					<p class="noticeCard_body_text">firebaseの動作確認</p>
 				</div>
 			</a>
 		</li>
+
 		<li class="noticeCard_list_item">
-			<a href="https://www.cyberagent.co.jp/news/detail/id=29467">
+			<a href="">
 				<div class="noticeCard_imgBox">
 					<div class="noticeCard_imgBox_inner">
-						<img alt="" src="https://placehold.jp/24/C5E898/ffffff/310x175.png?text=画像2">
+						<img alt="" src="https://placehold.jp/24/0766AD/ffffff/310x175.png?text=画像1">
 					</div>
 				</div>
 				<div class="noticeCard_list_body">
 					<div class="noticeCard_body_meta">
-						<p class="noticeCard_body_badge">プレスリリース</p>
+						<p class="noticeCard_body_badge">機能実装</p>
 						<p class="noticeCard_body_date">
-							<time datetime="2023-10-26">2023年10月26日</time>
+							<time datetime="2023-11-02">2023年12月18日</time>
 						</p>
 					</div>
-					<p class="noticeCard_body_text">製作した機能を書く<br/>製作した機能を書く<br/>製作した機能を書く<br/>製作した機能を書く<br/>製作した機能を書く<br/>製作した機能を書く<br/>製作した機能を書く<br/>製作した機能を書く<br/></p>
+					<p class="noticeCard_body_text">laravel開発環境作成</p>
 				</div>
 			</a>
 		</li>
+		
 	</ul>
 </div>
